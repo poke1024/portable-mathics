5bf496c95e15c90386f45df8f21becdae05e86e7
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-21 12:59:32 +0200

    fixed for some environments

--- winpython-mathics/ipython_notebook.bat
+++ winpython-mathics/ipython_notebook.bat
@@ -5,8 +5,9 @@ set EXTRAS=C:\Users\windows\Desktop\data
 set NODE_PATH=%EXTRAS%\node_modules
 set SPACY_DATA=%EXTRAS%\spacy
 set TESSDATA_PREFIX=%EXTRAS%\tesseract
-set PATH=%PATH%;%WINPYDIR%\..\node;%EXTRAS%\tesseract
+set PATH=%WINPYDIR%\..\node;%PATH%;%EXTRAS%\tesseract
 
+"%WINPYDIR%\Scripts\pip.exe" -e "%WINPYDIR%\..\imathics"
 "%WINPYDIR%\python.exe" "%WINPYDIR%\..\scripts\launch.py"
 
 cd/D "%USERPROFILE%"
