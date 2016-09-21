2e1a64d78db25414b5b2ab131cb1f930eaf4b170
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-21 13:30:58 +0200

    more fixes

--- winpython-mathics/ipython_notebook.bat
+++ winpython-mathics/ipython_notebook.bat
@@ -1,13 +1,15 @@
 @echo off
 call "%~dp0env_for_icons.bat"
 
-set EXTRAS=C:\Users\windows\Desktop\data
+set EXTRAS=%WINPYDIR%\..\data
 set NODE_PATH=%EXTRAS%\node_modules
 set SPACY_DATA=%EXTRAS%\spacy
 set TESSDATA_PREFIX=%EXTRAS%\tesseract
 set PATH=%WINPYDIR%\..\node;%PATH%;%EXTRAS%\tesseract
 
-"%WINPYDIR%\Scripts\pip.exe" -e "%WINPYDIR%\..\imathics"
+cd "%WINPYDIR%\..\imathics"
+"%WINPYDIR%\python.exe" setup.py install
+
 "%WINPYDIR%\python.exe" "%WINPYDIR%\..\scripts\launch.py"
 
 cd/D "%USERPROFILE%"
