5c336759dfaf4edfe92639268f27d59255fd5d2d
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-26 09:15:48 +0200

    fixes several problems

--- winpython-mathics/ipython_notebook.bat
+++ winpython-mathics/ipython_notebook.bat
@@ -1,16 +1,20 @@
 @echo off
 call "%~dp0env_for_icons.bat"
 
-set EXTRAS=%WINPYDIR%\..\data
-set NODE_PATH=%EXTRAS%\node_modules
+set EXTRAS=%WINPYDIR%\..\..\Data
+set NLTK_DATA=%EXTRAS%\nltk_data
+REM set NODE_PATH=%EXTRAS%\node_modules
 set SPACY_DATA=%EXTRAS%\spacy
 set TESSDATA_PREFIX=%EXTRAS%\tesseract
 set PATH=%WINPYDIR%\..\node;%PATH%;%EXTRAS%\tesseract
 
-cd "%WINPYDIR%\..\imathics"
-"%WINPYDIR%\python.exe" setup.py install
+set HOME=%USERPROFILE%
+set JUPYTER_DATA_DIR=%USERPROFILE%
+rem cd "%WINPYDIR%\..\imathics"
+rem "%WINPYDIR%\python.exe" setup.py install
 
 "%WINPYDIR%\python.exe" "%WINPYDIR%\..\scripts\launch.py"
+jt.exe -t chesterish
 
 cd/D "%USERPROFILE%"
 "%WINPYDIR%\scripts\jupyter-notebook.exe" %*
